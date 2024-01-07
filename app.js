db8a568fb4aaf726371c031e5977a801a2daf1cc
unknown <mh2001harjani@gmail.com>
2024-01-07 19:19:00 +0530

    corrected app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 //=======================================================================================================================
@@ -14,7 +14,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({ path: './env' })
 //==========================================================================================================================
-//MIDDLEWARES
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -26,30 +26,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// error handler
 app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-//======================================================================================================================================
+//====================================================================================================================
 //CONNECTION WITH THE DATABASE
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => { console.log('CONNECTED TO DATABASE :)') })
   .catch((err) => { console.log('CONNECTION TO DATABASE FAILED :(', err) })
 
 
-
-
-
-
-
-
-//====================================================================================================================================
+//==========================================================================================================================
 module.exports = app;
